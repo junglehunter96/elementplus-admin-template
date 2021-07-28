9a816afd5cc88b25328e0ee2766dfc1fefd4767b
周鑫 <zhouxin@zhufaner.com>
2021-07-28 19:17:58 +0800

    feat:路由改造

---
+++ src/router/modules/permission.js
@@ -0,0 +1,20 @@
+export default {
+  id: 5,
+  parentId: 0,
+  name: 'Permission',
+  path: '/Permission',
+  component: 'Layout',
+  redirect: '/Permission/Directive',
+  meta: { title: '权限管理', icon: 'el-icon-phone', alwaysShow: true },
+  children: [
+    {
+      id: 50,
+      parentId: 5,
+      name: 'Directive',
+      path: '/Permission/Directive',
+      component: 'Directive',
+      meta: { title: '指令管理', icon: 'el-icon-goods' },
+    },
+  ],
+};
+
