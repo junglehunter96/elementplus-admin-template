ef37a61b8ba7847c8c90724d415d4dc07710c6a8
周鑫 <zhouxin@zhufaner.com>
2021-07-29 11:14:21 +0800

    styl: lint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,18 +1,21 @@
 module.exports = {
-    processors: [],
-    plugins: [],
-    extends: "stylelint-config-standard", // 这是官方推荐的方式
-    ignoreFiles: ["node_modules/**", "dist/**"],
-    rules: {
-        "at-rule-no-unknown": [ true, {
-            "ignoreAtRules": [
-                "responsive",
-                "tailwind"
-            ]
-        }],
-        "indentation": 4,        // 4个空格
-        "selector-pseudo-element-no-unknown": [true, {
-            "ignorePseudoElements": ["v-deep"]
-        }]
-    }
-}
+  processors: [],
+  plugins: [],
+  extends: 'stylelint-config-standard', // 这是官方推荐的方式
+  ignoreFiles: ['node_modules/**', 'dist/**'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['responsive', 'tailwind'],
+      },
+    ],
+    indentation: 4, // 4个空格
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
+  },
+};
